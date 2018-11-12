4e4cad4e54c00d727b33f0522ea6f9dfde19b7f3
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-11-12 19:55:20 -0200

    :zap: use inline export on ValidatorError module

--- src/ValidatorError.ts
+++ src/ValidatorError.ts
@@ -1,12 +1,10 @@
 /**
  * A valite's validator error class.
  */
-class ValidatorError extends Error {
+export default class ValidatorError extends Error {
   constructor (message: string) {
     super(message);
     this.name = 'ValidatorError';
     this.message = message;
   }
 }
-
-export default ValidatorError;
