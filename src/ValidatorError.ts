03451481ca9f4f6d455cba724319f4b496bb2659
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-11-13 16:10:02 -0200

    :wrench: upgrade dependencies and improve bundle

--- src/ValidatorError.ts
+++ src/ValidatorError.ts
@@ -2,12 +2,9 @@
  * A valite's validator error class.
  */
 export default class ValidatorError extends Error {
-  __proto__: Error;
-
   constructor (message: string) {
     super(message);
     this.name = 'ValidatorError';
     this.message = message;
-    this.__proto__ = new.target.prototype;
   }
 }
