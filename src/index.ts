2ccd6990a95aef1cda865f51c72c480ead3c7506
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-11-13 12:18:12 -0200

    :zap: use expressions and promises instead of async/await
    close #6

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,14 @@ import {
 export { Message, MessageSchema, ValidatorError };
 
 /**
- * Get an Array of properties from a given object.
+ * A key from an object.
  */
-const keys = Object.keys as <T = any> (object: T) => Extract<keyof T, string>[];
+type Key <T = any> = Extract<keyof T, string>;
+
+/**
+ * Get an Array of keys from a given object.
+ */
+const keys = Object.keys as <T = any> (object: T) => Key<T>[];
 
 /**
  * A function which receives a value and return true or an error message.
@@ -20,31 +25,25 @@ const keys = Object.keys as <T = any> (object: T) => Extract<keyof T, string>[];
 export type Validator = (value: any) => string | true | Promise<string | true>;
 
 /**
- * A schema of property names and their validators.
- */
-export type ValidatorSchema = { [property: string]: Array<Validator>; };
-
-/**
  * Sequentially executes validators over the value. Returns the first error
  * message or `null` otherwise.
  * @param value
  * @param validators
  */
-export const validate = async (
+export const validate = (
   value: any,
-  [validator, ...validators]: Array<Validator> = [],
-): Promise<Message> => {
-  try {
-    if (typeof validator !== 'function')
-      return null;
-    const message = await validator(value);
-    return isMessage(message) ? message : validate(value, validators);
-  } catch (error) {
-    if (error instanceof ValidatorError)
-      throw error;
-    throw new ValidatorError('Error on validator: ' + error && error.message);
-  }
-};
+  [validator, ...validators]: Array<Validator> = []
+): Promise<Message> => (
+  typeof validator !== 'function'
+    ? Promise.resolve(null)
+    : Promise.resolve(validator(value))
+      .then((message) => isMessage(message) ? message : validate(value, validators))
+);
+
+/**
+ * A schema of property names and their validators.
+ */
+export type ValidatorSchema = { [property: string]: Array<Validator>; };
 
 /**
  * Concurrenly validates an object with an schema. It executes validators for
@@ -52,17 +51,17 @@ export const validate = async (
  * @param object
  * @param schema
  */
-export const validateObject = async <T extends ValidatorSchema> (
+export const validateObject = <T extends ValidatorSchema> (
   object: object,
   schema: T,
 ): Promise<MessageSchema<T>> => {
   const errors = Object.create(null) as MessageSchema<T>;
-  const toResolution = async (property: string): Promise<void> => {
-    const value = get(object, property);
-    errors[property] = await validate(value, schema[property]);
-  };
-  await Promise.all(keys(schema).map(toResolution));
-  return errors;
+  const resolutions = keys(schema).map((key): Promise<void> => (
+    validate(get(object, key), schema[key]).then((message) => {
+      errors[key] = message;
+    })
+  ));
+  return Promise.all(resolutions).then(() => errors);
 };
 
 /**
@@ -71,9 +70,8 @@ export const validateObject = async <T extends ValidatorSchema> (
  */
 export const isValid = (
   error: Message | MessageSchema<any>,
-): boolean => {
-  if (!isMessageSchema(error))
-    return !isMessage(error);
-  const isError = (property: string) => typeof error[property] === 'string';
-  return !keys(error).some(isError);
-};
+): boolean => (
+  !isMessageSchema(error)
+    ? error === null
+    : keys(error).every((key) => error[key] === null)
+);
